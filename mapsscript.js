9ce0df1439bc55b988335cb64c3dd515dbf54939
rafik taamma <b.taamma@esi-sba.dz>
2020-07-18 16:59:48 +0100

    first commit

---
+++ mapsscript.js
@@ -0,0 +1,377 @@
+
+
+
+
+    var tab_info=[
+        {
+            TITLE: 'siege de saida',
+            HEADING: 'saida title ',
+            image: 'saida.jpg',
+            long: '0.151802',
+            lat: '34.830604',
+            content: 'ville d\'Algérie et chef-lieu de la wilaya de Saïda'
+        },
+        {
+            TITLE: 'siege d\'alger',
+            HEADING: 'alger title',
+            image: 'alger.jpg',
+            long: '3.086029',
+            lat: '36.730160',
+            content: 'ville d\'alger '
+        },
+        {
+            TITLE: 'siege d\'oran ',
+            HEADING: 'daira oran title',
+            image: 'oran.jpg',
+            long: '-0.6492976',
+            lat: '35.7032751',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege d\'oran ',
+            HEADING: 'Bir El Djir',
+            image: 'oran2.jpg',
+            long: '-0.567299',
+            lat: '35.720945',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege d\'oran ',
+            HEADING: 'ES senia',
+            image: 'oran3.jpg',
+            long: '-0.605483',
+            lat: '35.653707',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '3.1077277',
+            lat: '36.6229795',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '3.2885455',
+            lat: '36.7259091',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '3.1854975',
+            lat: '36.7206251',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '-0.608668',
+            lat: '35.709984',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '-0.563307',
+            lat: '35.690171',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '0.149499',
+            lat: '35.390412',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '-0.567169',
+            lat: '34.754589',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '1.509247',
+            lat: '35.159775',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '5.736237',
+            lat: '34.862834',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '7.079315',
+            lat: '35.417594',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '3.592529',
+            lat: '32.375322',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '1.153564',
+            lat: '32.634749',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '4.471436',
+            lat: '36.009117',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '5.723877',
+            lat: '32.133757',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '7.591553',
+            lat: '36.681636',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '6.580811',
+            lat: '36.769692',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '5.152588',
+            lat: '36.540536',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '2.779541',
+            lat: '35.848987',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '-1.307373',
+            lat: '34.953493',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        },
+        {
+            TITLE: 'siege test',
+            HEADING: 'heading test',
+            image: 'test.jpg',
+            long: '0.359802',
+            lat: '35.923532',
+            content: 'orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis tortor malesuada elit tincidunt maximus. Nulla elementum neque erat,'
+        }
+    ];
+     /*var zoomFluid;
+
+    function zoomTo(map){
+        //console.log(zoomFluid);
+        console.log(zoomFluid)
+        if(zoomFluid >= 12) return 0;
+        else {
+            zoomFluid ++;
+            console.log(zoomFluid);
+            map.setZoom(zoomFluid);
+            setTimeout(function () {
+                zoomTo(map)
+            }, 100);
+        }
+    }*/
+
+    function smoothZoom (map, max, cnt , callback )  {
+        if (cnt >= max) {
+            console.log("fin")
+            if(callback) callback();
+            return;
+        }
+        else {
+            z = google.maps.event.addListener(map, 'zoom_changed', function(event){
+                google.maps.event.removeListener(z);
+                smoothZoom(map, max, cnt + 1 , callback);
+            });
+            setTimeout(function(){map.setZoom(cnt)}, 80); // 80ms is what I found to work well on my system -- it might not work well on all systems
+        }
+    }
+    function createContent(content_info){
+        var Contentfrominfo =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        "</div>" +
+        '<h1 id="firstHeading" class="firstHeading">'+ content_info.HEADING +'</h1>' +
+        '<img src=' + '\"' + content_info.image + '\"' + ' width="100%" height="80%">'  +
+        '<div id="bodyContent">' +
+        "<p> "+ content_info.content +" </p> " +
+        
+        "</div>" +
+        "</div>"; 
+
+
+        return Contentfrominfo;
+    }
+    function createMarker(content_info , map){
+        return new google.maps.Marker({
+            position:  new google.maps.LatLng(parseFloat(content_info.lat),parseFloat(content_info.long)),
+               /* lat: parseFloat(content_info.lat),
+                lng: parseFloat(content_info.long)
+              },*/
+            map: map,
+            title: content_info.TITLE
+          });
+
+    }
+
+    function initMap() {
+      var uluru = {
+        lat: 34.849210,
+        lng: 3.704308
+      };
+     
+      
+      var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: uluru,
+        /*restriction: {
+            latLngBounds: {
+            east: 12.046711,
+            north: 37.165815,
+            south: 18.728420,
+            west:  -8.667581
+            },
+        strictBounds: false
+      }*/}
+      );
+
+      
+      /*for (var item in tab_info) {
+           
+
+            createMarker(tab_info[item] ,map).addListener("click", function() {
+                map.setZoom(8);
+                map.setCenter(marker.getPosition());
+                new google.maps.InfoWindow({
+                    content: createContent(tab_info[item]),
+                    maxWidth: 300 ,
+                    pixelOffset : 0 
+                  }).open(map, this);
+                
+              });
+        }*/
+        var infowindow = new google.maps.InfoWindow();
+        for (i = 0; i < tab_info.length; i++) {
+            marker = createMarker(tab_info[i], map);
+
+            google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                return function () {
+                    /*map.setZoom(7);*/
+                    /*setTimeout( function () {
+                        map.setCenter(marker.getPosition());
+                    } , 0)
+                    zoomFluid = map.getZoom();
+                    /!*setTimeout(function() {
+                        map.setZoom(12);
+                    }, 100);*!/
+                    //zoomTo(map);*/
+
+                    map = marker.getMap();
+                    map.panTo(marker.getPosition()); // set map center to marker position
+                    infowindow.setContent(createContent(tab_info[i]));
+                    infowindow.setOptions({maxWidth:250});
+
+                    smoothZoom(map, 13, map.getZoom(),function () {
+                        console.log("callback")
+                        infowindow.open(map, marker);
+                    });
+
+                }
+            })(marker, i));
+        }
+
+
+
+
+       /* var infowindow = new google.maps.InfoWindow({
+        content: createContent(tab_info[1]),
+        maxWidth: 300 ,
+        pixelOffset : 0 
+      });
+
+      
+      var marker = createMarker(tab_info[1],map);
+      marker.addListener("click", function() {
+        map.setZoom(8);
+        map.setCenter(marker.getPosition());
+        infowindow.open(map, marker);
+        
+      });
+
+
+      
+      var infowindow2 = new google.maps.InfoWindow({
+        content: createContent(tab_info[0]),
+        maxWidth: 300 ,
+        pixelOffset : 0 
+      });
+
+      
+      var marker2 = createMarker(tab_info[0],map);
+      marker2.addListener("click", function() {
+        map.setZoom(8);
+        map.setCenter(marker2.getPosition());
+        infowindow2.open(map, marker2);
+        
+      }); */ 
+    
+   /* var marker2 = new google.maps.Marker({
+        position: alger_position,
+        map: map,
+        title: "Algiers"
+      });
+      marker2.addListener("click", function() {
+        map.setZoom(10);
+        map.setCenter(marker2.getPosition());
+        infowindow.open(map, marker2);
+        
+      });*/
+    }
